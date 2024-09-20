0385af79d60df35b963e6cc458c2e00d67c9fb33
Carlos Barrios <cabeee@rit.edu>
2024-09-20 12:56:42 -0400

    generate regular forwarding interests before the shortcutOPT interests.

--- daemon/fw/forwarder.hpp
+++ daemon/fw/forwarder.hpp
@@ -214,6 +214,10 @@ private:
   void
   setExpiryTimer(const shared_ptr<pit::Entry>& pitEntry, time::milliseconds duration);
 
+  void
+  sendShortcutOPTinterests(const Interest& interest, const FaceEndpoint& ingress,
+                     const shared_ptr<pit::Entry>& pitEntry);
+
   /** \brief Insert Nonce to Dead Nonce List if necessary.
    *  \param upstream if null, insert Nonces from all out-records;
    *                  if not null, insert Nonce only on the out-records of this face
